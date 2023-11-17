1ed74bf463ca06c3660a8603bafe8e03320e6ec4
NeoLez <66876432+NeoLez@users.noreply.github.com>
2023-11-17 19:31:04 -0300

    Consultas para uso en interfaces

--- src/main/java/org/grupo2b/proyectodisenio/dao/direccion/ProvinciaDAOPSQL.java
+++ src/main/java/org/grupo2b/proyectodisenio/dao/direccion/ProvinciaDAOPSQL.java
@@ -7,7 +7,9 @@ import org.grupo2b.proyectodisenio.dao.DAOManager;
 import org.grupo2b.proyectodisenio.logica.direccion.Provincia;
 import org.hibernate.query.Query;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Stream;
 
 public class ProvinciaDAOPSQL {
 
@@ -20,6 +22,32 @@ public class ProvinciaDAOPSQL {
 
         Query<Provincia> query = DAOManager.getSession().createQuery(cr);
         return query.getResultList();
+    }
+
+    public static List<String> getStringsProvincias(){
+        CriteriaBuilder cb = DAOManager.getSession().getCriteriaBuilder();
+        CriteriaQuery<Provincia> cr = cb.createQuery(Provincia.class);
+        Root<Provincia> root = cr.from(Provincia.class);
+
+        cr.select(root);
+
+        Query<Provincia> query = DAOManager.getSession().createQuery(cr);
+        List<String> strings = new ArrayList<>();
+        for (Provincia p : query.getResultList())
+            strings.add(p.getNombre());
+
+        return strings;
+    }
+
+    public static Provincia getFromNombre(String nombre){
+        CriteriaBuilder cb = DAOManager.getSession().getCriteriaBuilder();
+        CriteriaQuery<Provincia> cr = cb.createQuery(Provincia.class);
+        Root<Provincia> root = cr.from(Provincia.class);
+
+        cr.select(root).where(cb.like(root.get("nombre"), nombre));
+
+        Query<Provincia> query = DAOManager.getSession().createQuery(cr);
+        return query.getSingleResult();
     }
 
 }
