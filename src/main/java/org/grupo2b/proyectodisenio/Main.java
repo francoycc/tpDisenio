ba1e27619b7e9628d028fcb141a68d1e45d184d8
NeoLez <66876432+NeoLez@users.noreply.github.com>
2023-11-23 00:40:05 -0300

    Pantalla de carga

--- src/main/java/org/grupo2b/proyectodisenio/Main.java
+++ src/main/java/org/grupo2b/proyectodisenio/Main.java
@@ -1,10 +1,14 @@
 package org.grupo2b.proyectodisenio;
 
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 import org.grupo2b.proyectodisenio.carga_datos.CargadorProvinciaLocalidad;
 import org.grupo2b.proyectodisenio.dao.DAOManager;
 import org.grupo2b.proyectodisenio.carga_datos.Objetos;
@@ -18,16 +22,19 @@ import org.grupo2b.proyectodisenio.logica.vehiculo.GestorKmPorAnio;
 import org.grupo2b.proyectodisenio.logica.vehiculo.GestorVehiculos;
 import org.grupo2b.proyectodisenio.logica.vehiculo.Vehiculo;
 
+import java.io.IOException;
 import java.time.LocalDate;
 import java.util.Objects;
 import java.util.Optional;
 
 public class Main extends Application {
     @Override
-    public void start(Stage stage) {
+    public void start(Stage stage) throws InterruptedException {
         try {
-            AnchorPane root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("interfaz/Login.fxml")));
+            StackPane root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("interfaz/PantallaCarga.fxml")));
             Scene scene = new Scene(root);
+            stage.initStyle(StageStyle.TRANSPARENT);
+            scene.setFill(Color.TRANSPARENT);
             stage.setTitle("EL ASEGURADO");
             stage.setResizable(false);
             stage.setScene(scene);
@@ -35,15 +42,38 @@ public class Main extends Application {
         } catch (Exception e) {
             e.printStackTrace();
         }
+
+        Thread t = new Thread(){
+            @Override
+            public void run() {
+                super.run();
+                //CargadorProvinciaLocalidad.cargar();
+                DAOManager.init();
+                //Objetos.save();
+
+                Platform.runLater(new Runnable() {
+                    @Override
+                    public void run() {
+                        AnchorPane root = null;
+                        try {
+                            root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("interfaz/Login.fxml")));
+                        } catch (IOException e) {
+                            throw new RuntimeException(e);
+                        }
+                        Scene scene = new Scene(root);
+                        Stage stage2 = new Stage();
+                        stage2.setTitle("EL ASEGURADO");
+                        stage2.setResizable(false);
+                        stage2.setScene(scene);
+                        stage2.show();
+                        stage.close();
+                    }
+                });
+            }
+        };
+        t.start();
     }
     public static void main(String[] args) {
-        //CargadorProvinciaLocalidad.cargar();
-        //System.out.println(CargadorProvinciaLocalidad.mapaLocalidades.get(445));
-        DAOManager.init();
-
-        //Objetos.save();
-        
-
-        launch();
+         launch();
     }
 }
